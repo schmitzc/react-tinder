ea061dfeaadb4083f8909bfc01dfbfcfb108b69f
Cassie Koomjian <schmitzc@gmail.com>
2016-06-23 13:53:46 -0700

    Fetch weather data when city submitted

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '585231833e8ef89088f9c3157cb4743e';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
